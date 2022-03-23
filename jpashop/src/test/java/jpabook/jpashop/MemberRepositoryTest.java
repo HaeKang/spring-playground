2cbb3c9c67d98af7fb3e4ed8f7775b9f5d846921
haekang <ydjm1027@naver.com>
2022-03-23 21:46:33 +0900

    day3
    DB Setting & Test case

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 테스트케이스 만들기 : 클래스명 -> alt + enter
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+    
+    @Test
+    @Transactional
+//    @Rollback(false)
+    public void testMember () throws Exception{
+
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        // 변수 뽑기 단축키 ctrl alt v
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
